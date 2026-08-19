76613a6508cb8a13d1c50f04ce7934008f3b55fe
Naveen <naveenshijo@gmail.com>
2026-08-19 23:03:12 +0530

    Fixed issues

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 
-app = FastAPI() 
+app = FastAPI()
 
 @app.get("/hello")
 def get_hello():
+    """ Simply a fastapi endpoint """
     return {'response': 'hello world'}
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,12 +2,14 @@
 # It operates relative to the directory where you execute the command
 
 
-from fastapi.testclient import TestClient 
-from src.main import app  # Here the context of the file is relative to what was passed when running the command 'pytest'
+from fastapi.testclient import TestClient
+from src.main import app  # Here the context of the file is relative 
+# to what was passed when running the command 'pytest'
 
 client = TestClient(app)
 
 def test_read_root():
+    """ testing a fastapi endpoint """ 
     response = client.get("/hello")
     assert response.status_code == 200
     assert response.json() == {'response':'hello world'}
