3a24d93139a3401d3314b4ba00d40f7c57a8b258
Naveen <naveenshijo@gmail.com>
2026-08-19 21:56:44 +0530

    First commit

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+# RUN:   python -m pytest
+# It operates relative to the directory where you execute the command
+
+
+from fastapi.testclient import TestClient 
+from src.main import app  # Here the context of the file is relative to what was passed when running the command 'pytest'
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/hello")
+    assert response.status_code == 200
+    assert response.json() == {'response':'hello world'}
